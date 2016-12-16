65469a68ef1b95cbe60ff39dac8f875e7b762348
nhaouari <nassib.haouari@sarbacane.com>
2016-12-16 16:50:14 +0100

    add angular step

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,9 +11,10 @@
 angular
   .module('therapyEvolutionApp', [
     'ui.router',
-    'ngAnimate'
+    'ngAnimate',
+    'angular-steps'
   ])
-  .config(function($stateProvider, $urlRouterProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.when('/dashboard', '/dashboard/overview');
     $urlRouterProvider.otherwise('/login');
@@ -24,33 +25,38 @@ angular
         url: '',
         templateUrl: 'views/base.html'
       })
-        .state('login', {
-          url: '/login',
-          parent: 'base',
-          templateUrl: 'views/login.html',
-          controller: 'LoginCtrl'
-        })
-        .state('subscribe', {
-          url: '/subscribe',
-          parent: 'base',
-          templateUrl: 'views/subscribe.html',
-          controller: 'SubscribeCtrl'
-        })
-        .state('dashboard', {
-          url: '/dashboard',
-          parent: 'base',
-          templateUrl: 'views/dashboard.html',
-          controller: 'DashboardCtrl'
-        })
-          .state('overview', {
-            url: '/overview',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/overview.html'
-          })
-          .state('reports', {
-            url: '/reports',
-            parent: 'dashboard',
-            templateUrl: 'views/dashboard/reports.html'
-          });
+      .state('login', {
+        url: '/login',
+        parent: 'base',
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .state('subscribe', {
+        url: '/subscribe',
+        parent: 'base',
+        templateUrl: 'views/subscribe.html',
+        controller: 'SubscribeCtrl'
+      })
+      .state('dashboard', {
+        url: '/dashboard',
+        parent: 'base',
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardCtrl'
+      })
+      .state('overview', {
+        url: '/overview',
+        parent: 'dashboard',
+        templateUrl: 'views/dashboard/overview.html'
+      })
+      .state('reports', {
+        url: '/reports',
+        parent: 'dashboard',
+        templateUrl: 'views/dashboard/reports.html'
+      })
+      .state('tests', {
+        url: '/tests',
+        parent: 'dashboard',
+        templateUrl: 'views/dashboard/tests.html'
+      });
 
   });
