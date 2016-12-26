5a9100c70412753d63dcef15ace4422242b7e1fc
aliridha1510 <aliridha.haouaro@gmail.com>
2016-12-26 13:30:59 +0100

    login

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,14 +7,29 @@
  * # MainCtrl
  * Controller of therapyEvolutionApp
  */
-angular.module('therapyEvolutionApp')
-  .controller('LoginCtrl', function($scope, $location) {
+angular.module('therapyEvolutionApp', [])
+ 
+  .controller('LoginCtrl', ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
+        $scope.submit = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.submit($scope.user.email, $scope.user.password, function(response) {
+                if(response.success) {
+                    AuthenticationService.SetCredentials($scope.user.email, $scope.user.password);
+                    $location.path('/dashboard/tests');
+                } else {
+                    $scope.error = response.message;
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }]);
 
-    $scope.submit = function() {
 
-      $location.path('/dashboard');
 
-      return false;
-    }
 
-  });
+
+
+  
--- app/scripts/controllers/subscribe.js
+++ app/scripts/controllers/subscribe.js
@@ -8,13 +8,33 @@
  * Controller of therapyEvolutionApp
  */
 angular.module('therapyEvolutionApp')
-  .controller('SubscribeCtrl', function($scope, $location) {
-
+  .controller('SubscribeCtrl', function($scope, $location,$http) {
+    /// user is vide in scope//
+ $scope.user = {};
     $scope.submit = function() {
+    	/// Model //user.email="ali@ali.de";
+    	
+        //var user=new Object();
+       
+    	
 
-      $location.path('/dashboard');
+/// vorteil :
+    	$http.post("http://localhost:8082/cte/save",$scope.user)
+   .then(
+       function(response){
+         // success callback
+               $location.path('/dashboard');
 
-      return false;
+       }, 
+       function(response){
+         // failure callback
+
+       }
+    );
+
+
+
+
     }
 
   });
